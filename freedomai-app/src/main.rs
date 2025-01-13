137ad34559044b2342e985d0b91582fdb342b023
Nightmare1984 <gunther6000@gmail.com>
2025-01-13 01:40:48 -0500

    Small Starting Point

---
+++ freedomai-app/src/main.rs
@@ -0,0 +1,26 @@
+use gtk::prelude::*;
+
+fn main() {
+    // Initialize GTK
+    gtk::prelude::init();
+
+    // Create a window
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+    window.set_title("FreedomAI");
+    window.set_default_size(350, 70);
+
+    // Create a button
+    let button = gtk::Button::with_label("Click me!");
+    button.connect_clicked(move |_| {
+        println!("Button clicked!");
+    });
+
+    // Add the button to the window
+    window.add(&button);
+
+    // Show the window and its contents
+    window.show_all();
+
+    // Run the GTK main loop
+    gtk::main();
+}
